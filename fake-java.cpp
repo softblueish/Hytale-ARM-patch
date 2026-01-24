d74b9ab7184eb1cb83a1316d004018e8059dde20
Ashley R. <ashley.rooth@yahoo.com>
2026-01-25 00:20:12 +0100

    Added comments

--- fake-java.cpp
+++ fake-java.cpp
@@ -6,22 +6,28 @@
 int main(int argc, char* argv[]) {
     std::cout << "[ARM64 OPENJDK PATCH] Java runtime intercepted successfully." << std::endl;
 
+    // Gets the path to the ARM OpenJDK installation made by the patch
     std::string username = getenv("USERNAME");
     std::string executable = "C:\\Users\\" + username + "\\AppData\\Roaming\\Hytale\\install\\release\\package\\jre-native\\latest\\bin\\java.exe";
+
+    // Separates the arguments and puts them into a vector sorrounded by quotes
     std::vector<std::string> args;
     for (int i = 1; i < argc; ++i) {
         args.push_back("\"" + std::string(argv[i]) + "\"");
     }
 
+    // Joins the arguments back into a single command that can be ran
     std::string arguments = "";
     for (const auto& arg : args) {
         arguments += " " + arg;
     }
 
+    // Checks if HytaleServer.jar is being ran, and if so, launch WSL instead
     if(arguments.find("HytaleServer.jar") != std::string::npos) {
         std::cout << "[ARM64 OPENJDK PATCH] Detected HytaleServer.jar, launching WSL" << std::endl;
         executable = "wsl.exe --exec java";
 
+        // Make the paths WSL compatible
         for (auto& arg : args) {
             if(arg.find("C:\\") != std::string::npos) {
                 std::string wslPath = arg;
@@ -36,11 +42,13 @@ int main(int argc, char* argv[]) {
             }
         }
 
+        // Joins the arguments back into a single command that can be ran
         std::string arguments = "";
         for (const auto& arg : args) {
             arguments += " " + arg;
         }
 
+        // Replace localhost with the loopback IP address which can be accessed from the Host
         size_t ipPos = arguments.find("localhost");
         if (ipPos != std::string::npos) {
             arguments.replace(ipPos, 9, "127.0.0.1");
@@ -48,13 +56,16 @@ int main(int argc, char* argv[]) {
 
         std::cout << "[ARM64 OPENJDK PATCH] Executing command: " << executable << arguments << std::endl;
         std::cout << "[ARM64 OPENJDK PATCH] If you're wondering what this is, check out https://github.com/softblueish/Hytale-ARM-patch" << std::endl;
+
+        // Execute the command through WSL
         system((executable + arguments).c_str());
 
     } else {
         std::cout << "[ARM64 OPENJDK PATCH] Launching with native (ARM64) JDK instead." << std::endl;
         std::cout << "[ARM64 OPENJDK PATCH] Executing command: " << executable << arguments << std::endl;
         std::cout << "[ARM64 OPENJDK PATCH] If you're wondering what this is, check out https://github.com/softblueish/Hytale-ARM-patch" << std::endl;
-    
+        
+        // Execute the command with the ARM64 JDK through Host
         system((executable + arguments).c_str());
     }
     return 0;
